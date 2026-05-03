e571d674e988f1272e5586cb13fee43ed6082ad4
Niraj Nair <nirajnknair@gmail.com>
2026-05-03 14:05:10 +0530

    Fix custom keymaps not working

--- init.lua
+++ init.lua
@@ -84,9 +84,6 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
--- Load custom keymaps
-require 'custom.keymaps'
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -243,6 +240,9 @@ require('lazy').setup({
     },
   },
 })
+
+-- Load custom keymaps
+require 'custom.keymaps'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
