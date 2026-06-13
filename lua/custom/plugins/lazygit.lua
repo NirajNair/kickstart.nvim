82ecb3d5730276114a8419f1d872f51439e4724e
Niraj Nair <nirajnknair@gmail.com>
2026-06-13 16:48:37 +0530

    Add keybinds to open lazygit in inline/side-by-side view

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,6 +1,16 @@
 -- LazyGit integration
 -- https://github.com/kdheepak/lazygit.nvim
 
+vim.api.nvim_create_user_command('LazyGitSide', function()
+  vim.fn.setenv('DELTA_FEATURES', 'side-by-side')
+  vim.cmd 'LazyGit'
+end, {})
+
+vim.api.nvim_create_user_command('LazyGitInline', function()
+  vim.fn.setenv('DELTA_FEATURES', '')
+  vim.cmd 'LazyGit'
+end, {})
+
 ---@module 'lazy'
 ---@type LazySpec
 return {
@@ -20,6 +30,7 @@ return {
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>lg', '<cmd>LazyGitSide<cr>', desc = 'LazyGit (side-by-side)' },
+    { '<leader>lgi', '<cmd>LazyGitInline<cr>', desc = 'LazyGit (inline)' },
   },
 }
